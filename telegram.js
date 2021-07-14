afc8abc1b21d2102e1030689f23fd62396eade59
Inflexible88 <vinflexible@gmail.com>
2021-07-14 11:39:47 +0300

    bot

---
+++ telegram.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const TelegramApi = require('node-telegram-bot-api');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+const chatId = -1001582516206;
+const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
+const bot = new TelegramApi(token, { polling: true });
+
+app.set('port', PORT);
+app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+
+app.post('/bot', (req, res) => {
+  res.send(req.body);
+  const { topic = '', name = '', email = '', phone = '', msg = '' } = req.body;
+  console.log(`Имя: ${req.body.name} Телефон: ${req.body.phone}`);
+  try {
+    bot.sendMessage(
+      chatId,
+      `Тема: ${topic === '' ? 'Вопрос' : `Заказ - "${topic}"`} \nОт: ${name}${
+        phone ? '\nТелефон: ' + phone : ''
+      } ${email !== '' ? '\nEmail: ' + email : ''} ${msg !== '' ? '\nВопрос: ' + msg : ''}`,
+    );
+  } catch (e) {
+    console.log(`Что-то пошло не так. Телеграмм бот приказал долго жить...`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
